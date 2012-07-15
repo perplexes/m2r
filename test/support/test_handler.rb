1d7740f06dbd13969bc32bf114a34d9917a4d446
Paweł Pacana <pawel.pacana@gmail.com>
2012-07-15 13:46:11 +0200

    Introduce mocha in tests.

---
+++ test/support/test_handler.rb
@@ -0,0 +1,47 @@
+require 'm2r/handler'
+
+class TestHandler < M2R::Handler
+  attr_reader :called_methods
+  def initialize(connection)
+    super
+    @called_methods = []
+  end
+
+  def on_wait()
+    unless @called_methods.empty?
+      stop
+      return
+    end
+    @called_methods << :wait
+  end
+
+  def on_request(request)
+    @called_methods << :request
+  end
+
+  def process(request)
+    @called_methods << :process
+    return "response"
+  end
+
+  def on_disconnect(request)
+    @called_methods << :disconnect
+  end
+
+  def on_upload_start(request)
+    @called_methods << :start
+  end
+
+  def on_upload_done(request)
+    @called_methods << :done
+  end
+
+  def after_process(request, response)
+    @called_methods << :after
+    return response
+  end
+
+  def after_reply(request, response)
+    @called_methods << :reply
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,5 @@
 require 'minitest/autorun'
+require 'mocha'
 require 'm2r'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
