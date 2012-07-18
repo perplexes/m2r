9e45a4e7a5baa6b9311438308eeb7d9478503a35
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-07-18 22:05:27 +0000

    Fix test class name

--- test/headers_test.rb
+++ test/headers_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 module M2R
-  class HandlerTest < MiniTest::Unit::TestCase
+  class HeadersTest < MiniTest::Unit::TestCase
     def test_downcasing
       headers = Headers.new({"Content-Type" => "CT", "type" => "Ty"})
       assert_equal "CT", headers['content-type']
