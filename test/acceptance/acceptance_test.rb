69d37670d99bf036afb8f29a61f41079c43c5744
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-07-30 22:12:04 +0000

    Make 'rake test' pass
    * Discard all output
    
    * sleep for a second after killing externall processes to free
      zmq sockets so that they can properly bind in unit tests

--- test/acceptance/acceptance_test.rb
+++ test/acceptance/acceptance_test.rb
@@ -4,7 +4,7 @@ module M2R
   class AcceptanceTest < MiniTest::Unit::TestCase
 
     def test_requests
-      pid = Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: true)
+      pid = Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: true, out: "/dev/null", err: "/dev/null")
       sleep(1)
       user = TestUser.new(:driver => :mechanize)
       user.visit("/handler")
@@ -17,6 +17,7 @@ module M2R
       assert user.find("pre").text.include?("=:-----(   (  ( ( ( { {{{{{{")
     ensure
       Process.kill("SIGTERM", pid) if pid
+      sleep(1)
     end
   end
 
