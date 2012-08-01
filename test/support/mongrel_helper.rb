5da752a4eaf21ae067470c838f69df34bc9440cd
Paweł Pacana <pawel.pacana@gmail.com>
2012-08-01 12:07:38 +0200

    Kill proper process parent and wait after tests to shutdown.

--- test/support/mongrel_helper.rb
+++ test/support/mongrel_helper.rb
@@ -5,12 +5,13 @@ module MongrelHelper
 
   def setup
     check_mongrel
-    Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: true, out: "/dev/null", err: "/dev/null")
-    self.pid = read_pid_from_file('example/tmp/mongrel2.pid')
+    self.pid = Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: true, out: "/dev/null", err: "/dev/null")
+    wait_for_pid('example/tmp/mongrel2.pid')
   end
 
   def teardown
     Process.kill("SIGTERM", pid) if pid
+    sleep 1
   end
 
   def check_mongrel
@@ -34,4 +35,5 @@ module MongrelHelper
   rescue Timeout::Error
     raise "Unable to read PID from file #{pidfile}."
   end
+  alias :wait_for_pid :read_pid_from_file
 end
