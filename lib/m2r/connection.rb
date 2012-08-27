c0625d911a52ca66a2e158a4ff60e30244ca8c4c
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-08-27 21:27:21 +0000

    Updated doc

--- lib/m2r/connection.rb
+++ lib/m2r/connection.rb
@@ -1,27 +1,56 @@
 require 'm2r'
 
 module M2R
+  # Connection for exchanging data with mongrel2
   class Connection
 
+    # @param [ZMQ::Socket] request_socket socket for receiving requests
+    #   from Mongrel2
+    # @param [ZMQ::Socket] response_socket socket for sending responses
+    #   to Mongrel2
+    # @param [#parse] request_parser Object responsible for parsing Mongrel2
+    #   requests
+    # @api public
     def initialize(request_socket, response_socket, request_parser = Request)
       @request_socket  = request_socket
       @response_socket = response_socket
       @request_parser  = request_parser
     end
 
+    # For compatibility with [ConnectionFactory]
+    #
+    # @return self
+    # @api public
     def connection
       self
     end
 
+    # Returns parsed Mongrel2 request
+    #
+    # @note This is blocking call
+    # @api public
     def receive
       @request_socket.recv_string(msg = "")
       @request_parser.parse(msg)
     end
 
+    # Sends response to Mongrel2 for given request
+    #
+    # @param [Response, #to_s] response_or_string Response
+    #   for the request. Anything convertable to [String]
+    # @api public
     def reply(request, response_or_string)
       deliver(request.sender, request.conn_id, response_or_string.to_s)
     end
 
+    # Delivers data to multiple mongrel2 connections.
+    # Useful for streaming.
+    #
+    # @param [String] uuid Mongrel2 instance uuid
+    # @param [Array<String>, String] connection_ids Mongrel2 connections ids
+    # @param [String] data Data that should be delivered to the connections
+    #
+    # @api public
     def deliver(uuid, connection_ids, data)
       msg = "#{uuid} #{TNetstring.dump([*connection_ids].join(' '))} #{data}"
       @response_socket.send_string(msg)
--- lib/m2r/response/content_length.rb
+++ lib/m2r/response/content_length.rb
@@ -4,9 +4,9 @@ module M2R
   class Response
     # Adds Content-Length header based on body size
     # This is mostly required when you use bare
-    # Response class without any framework on top of it
-    # because HTTP clients require it when there is
-    # body in response. Otherwise clients hang out.
+    # Response class without any framework on top of it.
+    # HTTP clients require such header when there is
+    # body in response. Otherwise they hang out.
     #
     # @api public
     module ContentLength
