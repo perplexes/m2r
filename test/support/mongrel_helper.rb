91c78c4b5db2f08d8b94c534702c5dccddc3a313
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-09-10 20:53:05 +0000

    Display output from foreman processes to debug travis issues

--- test/support/mongrel_helper.rb
+++ test/support/mongrel_helper.rb
@@ -5,7 +5,7 @@ module MongrelHelper
 
   def setup
     check_mongrel
-    self.pid = Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: true, out: "/dev/null", err: "/dev/null")
+    self.pid = Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: true)
     wait_for_pid('example/tmp/mongrel2.pid')
   end
 
