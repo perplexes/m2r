bc5ed4a79f5b659d8b004970a6fce6ff0424b32f
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-11-04 01:25:12 +0100

    More Response testing

--- test/unit/response_test.rb
+++ test/unit/response_test.rb
@@ -27,11 +27,28 @@ module M2R
       assert_equal http, ok.to_s
     end
 
+    def test_response_with_header
+      ok = Response.new.body('data').header('X-Man', 'Wolverine')
+      ok.extend Response::ContentLength
+      http = "HTTP/1.1 200 OK\r\nx-man: Wolverine\r\ncontent-length: 4\r\n\r\ndata"
+      assert_equal http, ok.to_s
+    end
+
     def test_response_with_old_version
       ok = Response.new.http_version('HTTP/1.0').body('data')
       ok.extend Response::ContentLength
       http = "HTTP/1.0 200 OK\r\ncontent-length: 4\r\n\r\ndata"
       assert_equal http, ok.to_s
+    end
+
+    def test_getters
+      ok = Response.new.http_version(v = 'HTTP/1.0').status(s = 300).headers({'X-Man' => xman = 'Summers'}).header("X-Angel", angel = "Warren").body(data = 'data')
+      assert_equal v,     ok.http_version
+      assert_equal s,     ok.status
+      assert_equal xman,  ok.header("X-Man")
+      assert_equal angel, ok.header("X-Angel")
+      assert_equal data , ok.body
+      assert_equal 2,     ok.headers.size
     end
   end
 end
