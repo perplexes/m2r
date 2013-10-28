c5a5985e39a37fc04f51f22111cf3db481f8cc7f
Paweł Pacana <pawel.pacana@gmail.com>
2013-10-28 09:39:24 +0100

    Wait for socket instead of pid file in tests.

--- test/support/mongrel_helper.rb
+++ test/support/mongrel_helper.rb
@@ -7,7 +7,19 @@ module MongrelHelper
     check_mongrel
     `cd example && m2sh load -config mongrel2.conf --db config.sqlite`
     self.pid = Process.spawn("bundle exec foreman start --procfile=example/Procfile", pgroup: 0, out: "/dev/null", err: "/dev/null")
-    wait_for_pid('example/tmp/mongrel2.pid')
+    wait_until_mongrel_responsive
+  end
+
+  def wait_until_mongrel_responsive
+    client = Net::HTTP.new('localhost', 6767)
+    timeout(5) do
+      begin
+        client.start
+      rescue Errno::ECONNREFUSED
+        sleep(0.1)
+        retry
+      end
+    end
   end
 
   def teardown
@@ -19,22 +31,4 @@ module MongrelHelper
     skip("You must install mongrel2 to run this test") if `which mongrel2`.empty?
   end
 
-  def read_pid_from_file(pidfile)
-    pid = nil
-    Timeout.timeout(5) do
-      loop do
-        begin
-          pid = File.read(pidfile)
-          break unless pid.empty?
-        rescue Errno::ENOENT
-          sleep(0.25)
-          next
-        end
-      end
-      pid.to_i
-    end
-  rescue Timeout::Error
-    raise "Unable to read PID from file #{pidfile}."
-  end
-  alias :wait_for_pid :read_pid_from_file
 end
